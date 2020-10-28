5915a8585ab1b51131d751e2c265926818590415
Michael Klishevich <busation@gmail.com>
2020-10-28 11:07:52 +0300

    hello world scichart with react demo

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,34 @@ const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
-    mode: "production",
-    entry: "./src/index.js",
-    module: {
-        rules: []
-    },
-    resolve: {
-        extensions: [".js"]
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "build")
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: "src/index.html", to: "" },
-                { from: "node_modules/scichart/_wasm/scichart2d.data", to: "" },
-                { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "" }
-            ]
-        }),
-        new webpack.IgnorePlugin(/(fs)/)
-    ]
+  mode: "production",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/index.html", to: "" },
+        { from: "node_modules/scichart/_wasm/scichart2d.data", to: "" },
+        { from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "" },
+      ],
+    }),
+    new webpack.IgnorePlugin(/(fs)/),
+  ],
 };
